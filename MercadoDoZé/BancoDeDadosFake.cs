d25b2ecd97b9d813ae76e0e064689874e5ebe7e5
kevin <kevingomesm@gmail.com>
2023-12-12 17:27:42 -0300

    Adicionar arquivos de projeto.

---
+++ MercadoDoZé/BancoDeDadosFake.cs
@@ -0,0 +1,9 @@
+﻿using MercadinhoDoZe.Model;
+
+namespace MercadinhoDoZe
+{
+    public class BancoDeDadosFake
+    {
+        public static List<Produto> produtos = new List<Produto>();
+    }
+}
---
+++ MercadoDoZé/View/MenuView.cs
@@ -0,0 +1,55 @@
+﻿namespace MercadinhoDoZe.View
+{
+    public class MenuView
+    {
+        public void CriarMenuPrincipal()
+        {
+            int opcao = 0;
+
+            do
+            {
+                string menu = @"
+                    ### Mercadinho do Zé ###
+                    1- Clientes
+                    2- Produtos
+                    3- Vendas
+                    4- Sair
+                    Escolhe a opção desejada:
+                ";
+                Console.Write(menu);
+
+                try
+                {
+                    opcao = Int32.Parse(Console.ReadLine());
+                    switch (opcao)
+                    {
+                        case 1:
+                            Console.Clear();
+                            Console.WriteLine("Menu Cliente");
+                            break;
+                        case 2:
+                            Console.Clear();
+                            ProdutoView pV = new ProdutoView();
+                            pV.CriarMenu();
+                            break;
+                        case 3:
+                            Console.Clear();
+                            Console.WriteLine("Menu Venda");
+                            break;
+                        case 9:
+                            Environment.Exit(0);//Finalizar o programa
+                            break;
+                        default:
+                            throw new Exception("Opção selecionada não existe");
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Opção Inválida");
+                }
+
+            } while (opcao != 4);
+        }
+    }
+}
